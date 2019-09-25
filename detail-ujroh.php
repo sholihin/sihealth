269be6c7253f937fe3ca2f7d71d5cc4156531120
Mohamad Sholihin <mohamad.sholihin.it@gmail.com>
2019-09-25 17:04:42 +0700

    filter by date

--- detail-ujroh.php
+++ detail-ujroh.php
@@ -11,6 +11,30 @@
     </div>
     <div class="panel-body">
         <div class="table-responsive">
+            <div class="row">
+                <div class="col-md-12 form-inline">
+                    <form action="index.php" method="GET">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon">Tanggal Mulai</span>
+                                <input type="hidden" class="form-control" name="m" value="detail-ujroh">
+                                <input type="hidden" class="form-control" name="id" value="<?=$_GET[id]?>">
+                                <input type="text" class="form-control" name="min" id="min" value="<?=$_GET[min]?>">
+                            </div>
+                        </div>  
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon">Tanggal Selesai</span>
+                                <input type="text" class="form-control" name="max" id="max" value="<?=$_GET[max]?>">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary"><i class="glyphicon glyphicon-filter"></i> Filter</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <hr>
             <table id="table" class="table table-bordered table-hover table-striped">
             <thead>
                 <tr>
@@ -23,12 +47,25 @@
             </thead>
             <tbody>
             <?php
+            $max = $_GET[max];
+            $min = $_GET[min];
+
+            if($max != null){
+                $where = "AND tb_regristrasi.tanggal <= '$max'";
+            }elseif($min != null){
+                $where = "AND tb_regristrasi.tanggal >= '$min'";
+            }elseif($min != null && $max != null){
+                $where = "AND tb_regristrasi.tanggal >= '$min' AND tb_regristrasi.tanggal <= '$max'";
+            }else{
+                $where = "";
+            }
+
             $rows = $db->get_results("SELECT tb_detail_tindakan.id_detail_tindakan,tb_regristrasi.tanggal,tb_regristrasi.jenis_tindakan,tb_tindakan.kode_tindakan,tb_tindakan.harga,tb_tindakan.nama_tindakan,tb_tindakan.ujroh_senior,tb_tindakan.ujroh_junior,tb_dokter.level_terapis
                                         FROM tb_detail_tindakan
                                         RIGHT OUTER JOIN tb_regristrasi USING (kode_regristrasi)
                                         RIGHT OUTER JOIN tb_tindakan USING (kode_tindakan)
                                         RIGHT JOIN tb_dokter ON tb_dokter.kode_dokter = tb_detail_tindakan.terapis_id
-                                        WHERE terapis_id = 4 ORDER BY id_detail_tindakan DESC");
+                                        WHERE terapis_id = $_GET[id] $where ORDER BY id_detail_tindakan DESC");
             foreach($rows as $row):?>
             <tr>
                 <td><?=$row->id_detail_tindakan?></td>
@@ -60,9 +97,14 @@
 <script src="vendor/datatables/js/buttons.html5.min.js"></script>
 <script src="vendor/datatables/js/buttons.bootstrap.min.js"></script>
 <script src="vendor/datatables/js/buttons.print.min.js"></script>
-<script src="vendor/datatables/js/buttons.colVis.min.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<!-- <script src="https://cdn.datatables.net/plug-ins/1.10.19/filtering/row-based/range_dates.js"></script> -->
 <script>
     $(document).ready(function() {
+
         var table = $('#table').DataTable( {
             lengthChange: true,
             buttons: [ 'copy', 'excel', 'pdf' ],
@@ -100,6 +142,13 @@
                 );
             }
         } );
+
+        $('#min').datepicker({ dateFormat: 'yy-mm-dd'});
+        $('#max').datepicker({ dateFormat: 'yy-mm-dd'});
+
+        $('#min, #max').change(function () {
+            table.draw();
+        });
 
         table.buttons().container()
             .appendTo( '#table_wrapper .col-sm-6:eq(0)' );
